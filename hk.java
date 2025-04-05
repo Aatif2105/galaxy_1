c84cfab3fbde784845830ecb2d38ca7d5222eaf0
Aatif <aatifkhan2105@gmail.com>
2025-04-05 16:56:38 +0530

    done9

--- hk.java
+++ hk.java
@@ -2,5 +2,6 @@ public class hk {
     public static void main (String args []){
   System.out.println ("hello java");
   System.out.println ("hello css");
+  System.out.println ("hello HTML");
     }
 }
